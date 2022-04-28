7946a24ffb2cb2370326060754d752bdd8edb1c7
Chase <63676233+chaseetech@users.noreply.github.com>
2022-04-27 22:14:56 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+
+import random
+import discord
+import os
+from discord.ext import tasks
+from discord.ext.commands import Bot
+from discord.ext.commands import AutoShardedBot
+import time
+
+y = ["1", "2" ,"3", "4", "5", "6"]
+
+bot = Bot(command_prefix="/")
+
+
+bot = AutoShardedBot(command_prefix="/")
+@bot.command(name="hex")
+async def send_message(ctx):
+  c = bot.get_channel(937579766784065598) or (await bot.fetch_channel(937579766784065598))
+  await ctx.send(hex(random.randint(1, 999999999999999999999999999999999999999999)))       #Random dice>
+
+@bot.command(name="random")
+async def random_cmd(ctx):
+  c = bot.get_channel(937563323254321183) or (await bot.fetch_channel(937563323254321183))
+  await ctx.send(f"You have rolled a {random.randint(1, 11)}")
+
+@bot.command(name="hello")
+async def send_message():
+  c = bot.get_channel(929149843283275816) or (await bot.fetch_channel(929149843283275816))
+  await ctx.send("Hello there!")
+
+bot.run("token")
+
+
+
