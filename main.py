5eedb2e2916f1a24d00786565051c4fc9aa21780
Chase <63676233+chaseetech@users.noreply.github.com>
2022-06-06 19:43:54 -0600

    fix comments
    fixing comments, needs to have 1 space before code.

--- main.py
+++ main.py
@@ -16,16 +16,16 @@ bot = AutoShardedBot(command_prefix="/")
 @bot.command(name="hex")
 async def send_message(ctx):
   c = bot.get_channel(937579766784065598) or (await bot.fetch_channel(937579766784065598))
-  await ctx.send(hex(random.randint(1, 999999999999999999999999999999999999999999)))       #Hexadecimal generate
+  await ctx.send(hex(random.randint(1, 999999999999999999999999999999999999999999))) #Hexadecimal generate
 
 @bot.command(name="random")
 async def random_cmd(ctx):
-  c = bot.get_channel(937563323254321183) or (await bot.fetch_channel(937563323254321183))  #Random dice roll 
+  c = bot.get_channel(937563323254321183) or (await bot.fetch_channel(937563323254321183)) #Random dice roll 
   await ctx.send(f"You have rolled a {random.randint(1, 11)}")
 
 @bot.command(name="hello")
 async def send_message():
-  c = bot.get_channel(929149843283275816) or (await bot.fetch_channel(929149843283275816))  #Hello Command
+  c = bot.get_channel(929149843283275816) or (await bot.fetch_channel(929149843283275816)) #Hello Command
   await ctx.send("Hello there!")
 
 bot.run("token")
