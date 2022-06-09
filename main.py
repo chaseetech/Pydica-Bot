c0bcab18fa066e2e41a994e80b5a5a8c14166c4f
Chase <63676233+chaseetech@users.noreply.github.com>
2022-06-08 21:56:20 -0600

    Change command /random to /roll
    changing the command so it makes more sense when rolling a dice

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ async def send_message(ctx):
   c = bot.get_channel(937579766784065598) or (await bot.fetch_channel(937579766784065598))
   await ctx.send(hex(random.randint(1, 999999999999999999999999999999999999999999))) #Hexadecimal generate
 
-@bot.command(name="random")
+@bot.command(name="roll")
 async def random_cmd(ctx):
   c = bot.get_channel(937563323254321183) or (await bot.fetch_channel(937563323254321183)) #Random dice roll 
   await ctx.send(f"You have rolled a {random.randint(1, 11)}")
